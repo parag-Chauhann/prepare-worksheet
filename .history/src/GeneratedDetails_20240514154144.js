f6295add8f9c99720ce9a85557672ff25602dbfe
Parag Chauhan <paragchauhan33@gmail.com>
2024-05-14 16:11:09 +0530

    Updated

--- .history/src/GeneratedDetails_20240514154144.js
+++ .history/src/GeneratedDetails_20240514154144.js
@@ -6,7 +6,7 @@ const GeneratedDetails = ({ details }) => {
             <h2>Generated Details</h2>
             <p>Observation: {details.Observation}</p>
             <p>Element: {details.Element}</p>
-            <p>Legal Standards: {details['Legal Standards']}</p>
+            <p>Standard: {details['Relevant Standards']}</p>
             <p>Recommendation: {details.Recommendation}</p>
             <p>Risk Level: {details['Risk Level']}</p>
         </div>
---
+++ .history/src/GeneratedDetails_20240514154154.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const GeneratedDetails = ({ details }) => {
+    return (
+        <div className="generated-details">
+            <h2>Generated Details</h2>
+            <p>Observation: {details.observation}</p>
+            <p>Element: {details.element}</p>
+            <p>Standard: {details.standard}</p>
+            <p>Recommendation: {details.recommendation}</p>
+            <p>Risk Level: {details.risk_level}</p>
+
+        </div>
+    );
+};
+
+export default GeneratedDetails;
